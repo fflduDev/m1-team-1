2fe7795ddfb1af0bfc60f36a162ee1c24bfecc5e
Sofia Morgillo <sofia.morgillo@student.fairfield.edu>
2025-02-22 01:35:06 -0500

    commit

--- stack/StackTester.java
+++ stack/StackTester.java
@@ -4,12 +4,22 @@ public class StackTester {
 
 	public static void main(String[] args) {
 
-		Stack stackTester = new StackImpl();
+		Stack stackTester = new StackImpl(10);
 		stackTester.push("thing in stack");
+		
+		runTests(stackTester);
 
 	}
 	
 	public static void runTests(Stack stack) {
+	 	
+		stack.push("test");
+		stack.push("test2");
+		stack.display();
+	 	stack.pop();
+	 	stack.display();
+	 	System.out.println(stack.peek());
+	 	stack.display();
 	 	
 	}
 
