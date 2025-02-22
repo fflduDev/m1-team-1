6f3026dc9e6bf59126da671bb971687ff515054e
Sofia Morgillo <sofia.morgillo@student.fairfield.edu>
2025-02-22 01:32:44 -0500

    commit

--- queue/QueueTester.java
+++ queue/QueueTester.java
@@ -3,7 +3,16 @@ package queue;
 public class QueueTester {
 
 	public static void main(String[] args) {
-
+		QueueImpl q=new QueueImpl(10);
+		q.enQueue("test1");
+		q.enQueue("test2");
+		q.enQueue("test3");
+		q.display();
+		System.out.println("after enqueue");
+		q.deQueue();
+		q.display();
+		System.out.println("after dequeue");
+		System.out.println("peek: "+q.peek());
 	}
 
 }
